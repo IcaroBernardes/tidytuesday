c8bc9899e879d93ab7c02d04cb2ef5fd39fd1436
Icaro Bernardes <asaicaro@gmail.com>
2022-02-15 22:55:28 -0300

    week06 (complete) and week04 (ongoing)

--- 2022/week04/week04.R
+++ 2022/week04/week04.R
@@ -1,22 +1,63 @@
 # 0. Library and fonts management
 library(tidyverse)
 library(ggplot2)
-library(ViewPipeSteps) ## Allows the use of a printer pipe (%P>%) to show intermediate results
 library(showtext)
 library(tidytuesdayR)
 library(glue)
-library(fastDummies)
 library(ggtext)
-library(patchwork)
-library(ggimage)
-library(magick)
-library(plotrix)
+library(ggbump)
+library(tm)
+library(tidytext)
 
-## Adding Google Fonts
-font_add_google(name = "Lora", family = "lora") ### Sans Serif
-serif <- "lora"
+## Adding FontMeme.com fonts
+### Creates the "fonts" directory
+dir.create("2022/week04/fonts")
 
-## Allows the use of the downloaded Google Font
+### Adds the "fonts" directory to the search path
+sysfonts::font_paths("2022/week04/fonts")
+
+### Downloads and extracts the fonts (only regular styles if multiples)
+ft.url <- c("https://fontmeme.com/fonts/download/243426/grandstander.zip",
+            "https://fontmeme.com/fonts/download/202877/life-savers.zip")
+ft.path <- glue::glue("2022/week04/fonts/{basename(ft.url)}")
+purrr::map2(ft.url, ft.path,
+            function(.x,.y) {
+              
+              download.file(.x, .y)
+              
+              ft.files = unzip(.y, list = TRUE)
+              if (dim(ft.files)[1] > 1) {
+                ft.files = ft.files %>% 
+                  dplyr::filter(str_detect(tolower(Name), "regular")) %>% 
+                  dplyr::slice(1L)
+              }
+              ft.files = ft.files %>% dplyr::pull(Name)
+              
+              unzip(.y, files =  ft.files, exdir = "2022/week04/fonts")
+              
+            })
+
+### Gets the name of the font and adds it to the system
+purrr::map(ft.path,
+           function(.x) {
+             
+             ft.files = unzip(.x, list = TRUE)
+             if (dim(ft.files)[1] > 1) {
+               ft.files = ft.files %>% 
+                 dplyr::filter(str_detect(tolower(Name), "regular")) %>% 
+                 dplyr::slice(1L)
+             }
+             ft.files = ft.files %>% dplyr::pull(Name)
+             
+             ft.family = tolower(stringr::str_sub(ft.files, 1L, -5L))
+             
+             sysfonts::font_add(ft.family, glue::glue("2022/week04/fonts/{ft.files}"))
+             
+           })
+sans <- "grandstander-regular"
+serif <- "lifesavers-regular"
+
+## Allows the use of the downloadeded fonts
 ## To see the results updated, it's needed to call windows() or save the image
 showtext::showtext_auto()
 
@@ -25,8 +66,8 @@ ratings <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/ti
 details <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-01-25/details.csv')
 
 ## Gets the categories of each game
-categories <- details %>%
-  dplyr::select(yearpublished, boardgamecategory) %>%
+category_tokens <- details %>%
+  dplyr::select(yearpublished, boardgamecategory, id, description) %>%
   dplyr::mutate(boardgamecategory = stringr::str_sub(boardgamecategory, 2,-2),
                 boardgamecategory = stringr::str_remove_all(boardgamecategory, "'"),
                 boardgamecategory = stringr::str_remove_all(boardgamecategory, '"')) %>%
@@ -42,13 +83,13 @@ top_years <- details %>%
   dplyr::pull(yearpublished)
 
 ## Filters only the top5 categories of each year
-categories <- categories %>% 
+categories <- category_tokens %>% 
   dplyr::filter(yearpublished %in% top_years) %>% 
   dplyr::count(yearpublished, category) %>% 
   dplyr::group_by(yearpublished) %>% 
   dplyr::arrange(desc(yearpublished), desc(n)) %>% 
   dplyr::slice(1:5) %>% 
-  dplyr::mutate(order = seq(18, 2, -4)) %>% 
+  dplyr::mutate(order = seq(14, 2, -3)) %>% 
   dplyr::ungroup()
 
 ## Gets the set of most frequent categories for the given years and associates
@@ -57,12 +98,17 @@ categories <- categories %>%
 top_categories <- categories %>% 
   dplyr::count(category, name = "hits") %>% 
   dplyr::arrange(desc(hits)) %>% 
-  dplyr::mutate(fill = c("#c19e7a", "#ee7cff", "#77e641", "#53b1ff", "#fccc00",
-                         "#45b0d4", "#ff9e49", "#02d6bd", "#ff99a1", "#00c553",
-                         "#fff2c1", "#7fff93", "#eeffa9", "#b2ffb4")) %>% 
+  dplyr::mutate(color = c("#c19e7a", "#ee7cff", "#77e641", "#53b1ff", "#fccc00",
+                          "#54d4ff", "#ff9e49", "#02d6bd", "#ff99a1", "#00c553",
+                          "#fff2c1", "#7fff93", "#eeffa9", "#b2ffb4")) %>% 
   dplyr::mutate(names = stringr::str_remove_all(category, "[:punct:]")) %>% 
   dplyr::mutate(icons = glue::glue("2022/week04/icons/{names}.png")) %>% 
   dplyr::select(-hits, -names)
+
+## Makes sure each category has an "order" value
+## even in years they don't make to the top5
+categories <- categories %>% 
+  complete(yearpublished, category, fill = list(order = -10))
 
 ## Defines colors for the categories
 categories <- categories %>% 
@@ -72,83 +118,132 @@ categories <- categories %>%
 categories <- categories %>% 
   dplyr::mutate(label = glue::glue("<strong style = 'font-size:20pt;'>{n}:</strong> {category}"))
 
-## Obtains the link, name and average for the best rated games per year and category
-categories <- details %>% 
-  dplyr::left_join(ratings, by = "id") %>% 
+## Cleans and breaks down games descriptions by word
+stop <- tidytext::get_stopwords()$word
+describe <- category_tokens %>% 
   dplyr::filter(yearpublished %in% top_years) %>% 
-  dplyr::select(yearpublished, boardgamecategory, rank, primary, average, thumbnail, id) %>% 
-  dplyr::mutate(boardgamecategory = stringr::str_sub(boardgamecategory, 2,-2),
-                boardgamecategory = stringr::str_remove_all(boardgamecategory, "'"),
-                boardgamecategory = stringr::str_remove_all(boardgamecategory, '"')) %>%
-  tidytext::unnest_tokens(categname, boardgamecategory, token = "regex", pattern = ",") %>%
-  dplyr::mutate(categname = stringr::str_trim(categname, "both")) %>%
-  dplyr::filter(categname %in% top_categories$category) %>% 
-  dplyr::group_by(yearpublished) %>% 
-  tidyr::nest() %>% 
-  dplyr::full_join(categories) %>% 
+  dplyr::filter(category %in% top_categories$category) %>%
+  dplyr::mutate(description = tolower(description),
+                description = tm::removePunctuation(description),
+                description = tm::removeNumbers(description),
+                description = tm::stripWhitespace(description)) %>% 
+  tidytext::unnest_tokens(txt, description) %>% 
+  dplyr::filter(!(txt %in% stop))
+
+## Gets the most used words in descriptions in general
+top_descriptors <- describe %>% 
+  dplyr::distinct(id, txt, .keep_all = TRUE) %>% 
+  dplyr::count(txt) %>% 
+  dplyr::arrange(desc(n))
+
+## Elimintates the top 15 words used in general and gets
+## the most used words in descriptions in each category
+stop <- top_descriptors %>% 
+  dplyr::slice(1L:30L) %>% 
+  dplyr::pull(txt)
+descriptors <- describe %>% 
+  dplyr::filter(!(txt %in% stop)) %>% 
+  dplyr::count(category, txt) %>%
+  dplyr::group_by(category) %>% 
+  dplyr::arrange(category, desc(n)) %>% 
+  dplyr::slice(1L:15L) %>% 
+  dplyr::mutate(rank = 1:n()) %>% 
   dplyr::ungroup() %>% 
-  dplyr::mutate(
-    data = purrr::map2(
-      data, category,
-      ~.x %>% 
-        dplyr::filter(categname == .y) %>% 
-        dplyr::slice_min(rank)
-    )) %>% 
-  tidyr::unnest(cols = data)
+  tidyr::pivot_wider(names_from = category,
+                     values_from = c(txt, n))
 
-## Creates a temporary image to mask the thumbnails and saves it
-imgdim <- 64
-png(tf <- tempfile(fileext = ".png"), 1000, 1000)
-par(mar = rep(0,4), yaxs="i", xaxs="i")
-plot(0, type = "n", ylim = c(0,1), xlim=c(0,1), axes=F, xlab=NA, ylab=NA)
-plotrix::draw.circle(.5,0.5,.5, col="black")
-dev.off()
-mask <- magick::image_read(tf)
-mask <- magick::image_scale(mask, imgdim)
 
-## Downloads the thumbnails, resizes them, applies the mask and saves the
-## resulting images. Uncomment it if you need to modify the thumbnails 
-for (i in 1:dim(categories)[1]) {
-  
-  ### Gets the image with retina resolution
-  link <- categories$thumbnail[i]
-  img <- magick::image_read(link, density = 320)
-  
-  ### Gets info about dimensions of the image and scales and crops
-  ### it so the smallest size is the one defined at imgdim
-  info <- magick::image_info(img)
-  info <- min(info$width, info$height)
-  info <- imgdim/info
-  img <- magick::image_resize(img, geometry = glue::glue("{info*imgdim}x{info*imgdim}+0+0"))
-  img <- magick::image_crop(img, repage = TRUE,
-                            geometry = glue::glue("{imgdim}x{imgdim}+0+0"))
-  
-  ### Applies the mask and saves the image
-  img <- magick::image_composite(mask, img, operator = "minus")
-  magick::image_write(img, format = "png",
-                      path = glue::glue("2022/week04/thumbs/{categories$id[i]}.png"))
-  
-}
+## Gets coordinates for the icons on the timeline
+## (first appearance of the category on the top 5)
+first <- categories %>% 
+  na.exclude() %>% 
+  dplyr::group_by(category) %>% 
+  dplyr::slice(1L)
 
-## Associates the games to the masked thumbnails
-categories <- categories %>% 
-  dplyr::mutate(thumbnail = glue::glue("2022/week04/thumbs/{id}.png"))
+## Defines coordinates for the titles
+title <- tibble(
+  x = c(1996),
+  y = c(35,31),
+  size = c(70,25),
+  family = c(sans, serif),
+  label = c("Always trust the heart of the cards",
+            "Almost every board game in the BGG database has a category.<br>
+            They mostly represent elements of game mechanic 
+            <span style='color:#39c0ed'>(dices,</span> 
+            <span style='color:#c19e7a'>cards)</span> or themes 
+            <span style='color:#77e641'>(fantasy,</span> 
+            <span style='color:#ee7cff'>war)</span>.<br>
+            The timeline bellow shows (top to bottom) the Top 5 categories in regard to the number of lauched games each year.<br>
+            The tags bellow present the names of the categories and the total of launched games between 1996 and 2021."),
+)
+
+## Defines coordinates for the tag categories
+tags <- categories %>%
+  na.exclude() %>% 
+  dplyr::group_by(category) %>% 
+  dplyr::summarise(total = sum(n),
+                   color = unique(color),
+                   icons = unique(icons)) %>% 
+  dplyr::ungroup() %>% 
+  dplyr::mutate(label = glue::glue("{category}: *{total}*")) %>% 
+  dplyr::arrange(desc(total)) %>% 
+  dplyr::mutate(x = rep(seq(1996.2,2018.2,length.out = 7),2),
+                y = c(rep(22,7),rep(19,7)))
+
+## Defines coordinates for the values of the x-axis
+axisX <- tibble(
+  x = 1996:2021,
+  y = 15.7
+)
+
+## Defines coordinates for the values of the x-axis
+axisY <- tibble(
+  x = 1995.4,
+  y = seq(14, 2, -3),
+  label = glue::glue("{1:5}<sup>{c('st','nd','rd','th','th')}</sup>")
+)
 
 # 2. Plot making
 ## Defines some layout constants
-asp_ratio <- 0.2
+lnhgt <- 0.37
+asp_ratio <- 0.3
 
+## Creates the plot
 p <- categories %>% 
   ggplot() +
-  geom_tile(aes(x = yearpublished, y = order, fill = I(fill)),
-            width = 0.2, height = 1) +
+  
+  ### Places points and lines to mark the rank evolution of the categories
+  ggbump::geom_bump(aes(x = yearpublished, y = order, color = I(color)), size = 2) +
+  geom_point(aes(x = yearpublished, y = order, color = I(color)), size = 6) +
+  
+  ### Places the axes labels
+  ggtext::geom_richtext(aes(x = x, y = y, label = x), size = 17, color = "white",
+                        label.color = NA, fill = NA, family = serif, data = axisX) +
+  ggtext::geom_richtext(aes(x = x, y = y, label = label), size = 17,
+                        color = "white", hjust = 1, label.color = NA,
+                        fill = NA, family = serif, data = axisY) +
+  
+  
+  ggtext::geom_richtext(aes(x = x, y = y, label = label, size = I(size), family = family),
+                        hjust = 0, vjust = 1, color = "white", label.color = NA,
+                        fill = NA, lineheight = lnhgt, data = title) +
+  
+  ggtext::geom_textbox(aes(x = x, y = y, label = label, fill = I(color)),
+                       color = "black", box.color = NA, family = serif,
+                       halign = 0, hjust = 0, size = 13,
+                       box.padding = unit(c(5.5, 5.5, 5.5, 20), "pt"),
+                       width = unit(2.4, "inch"), data = tags) +
+  geom_point(aes(x = x, y = y, color = I(color)), size = 16, data = tags) +
+  ggimage::geom_image(aes(x = x, y = y, image = icons), size = 0.013,
+                      by = "width", asp = 8*asp_ratio, data = tags) +
+  
+  geom_point(aes(x = yearpublished, y = order, color = I(color)),
+             size = 11, data = first) +
   ggimage::geom_image(aes(x = yearpublished, y = order, image = icons),
-                      size = 0.003, by = "width", asp = 1/asp_ratio) +
-  ggimage::geom_image(aes(x = yearpublished+0.4, y = order, image = thumbnail),
-                      size = 0.01, by = "width", asp = 1/asp_ratio) +
-  scale_x_continuous(limits = c(1995.5, 2022)) +
-  scale_y_continuous(limits = c(0, 27)) +
-  coord_equal(ratio = asp_ratio, expand = FALSE) +
+                      size = 0.008, by = "width", asp = 8*asp_ratio,
+                      data = first) +
+  
+  coord_equal(xlim = c(1994.5, 2022.5), ylim = c(0, 37), ratio = asp_ratio, expand = FALSE) +
   theme_void() +
   theme(
     panel.background = element_rect(fill = "black", color = NA)
@@ -156,7 +251,7 @@ p <- categories %>%
 
 ## Saves the plot
 ggsave("2022/week04/categ_rank.png", plot = p, dpi = "retina",
-       width = 50, height = 50*asp_ratio, limitsize  = FALSE)
+       width = 30, height = 30*asp_ratio, limitsize  = FALSE)
 
 # # 3. Code for (maybe) another plot
 # ## Gets the categories of each game
@@ -194,3 +289,39 @@ ggsave("2022/week04/categ_rank.png", plot = p, dpi = "retina",
 #   dplyr::right_join(dummies) %>%
 #   dplyr::left_join(ratings, by = "id") %>%
 #   dplyr::select(id, description, playingtime, minage, owned, average, starts_with("category"))
+
+
+
+# ## Creates a temporary image to mask the thumbnails and saves it
+# imgdim <- 64
+# png(tf <- tempfile(fileext = ".png"), 1000, 1000)
+# par(mar = rep(0,4), yaxs="i", xaxs="i")
+# plot(0, type = "n", ylim = c(0,1), xlim=c(0,1), axes=F, xlab=NA, ylab=NA)
+# plotrix::draw.circle(.5,0.5,.5, col="black")
+# dev.off()
+# mask <- magick::image_read(tf)
+# mask <- magick::image_scale(mask, imgdim)
+# 
+# ## Downloads the thumbnails, resizes them, applies the mask and saves the
+# ## resulting images. Uncomment it if you need to modify the thumbnails 
+# for (i in 1:dim(categories)[1]) {
+#   
+#   ### Gets the image with retina resolution
+#   link <- categories$thumbnail[i]
+#   img <- magick::image_read(link, density = 320)
+#   
+#   ### Gets info about dimensions of the image and scales and crops
+#   ### it so the smallest size is the one defined at imgdim
+#   info <- magick::image_info(img)
+#   info <- min(info$width, info$height)
+#   info <- imgdim/info
+#   img <- magick::image_resize(img, geometry = glue::glue("{info*imgdim}x{info*imgdim}+0+0"))
+#   img <- magick::image_crop(img, repage = TRUE,
+#                             geometry = glue::glue("{imgdim}x{imgdim}+0+0"))
+#   
+#   ### Applies the mask and saves the image
+#   img <- magick::image_composite(mask, img, operator = "minus")
+#   magick::image_write(img, format = "png",
+#                       path = glue::glue("2022/week04/thumbs/{categories$id[i]}.png"))
+#   
+# }
---
+++ 2022/week06/week06.R
@@ -0,0 +1,202 @@
+# 0. Library and fonts management
+library(tidyverse)
+library(ggplot2)
+library(showtext)
+library(glue)
+library(scales)
+library(ggforce)
+library(ggtext)
+
+## Adding Google Fonts
+sysfonts::font_add_google(name = "Teko", family = "teko") ### Sans Serif
+sans <- "teko"
+
+## Allows the use of the downloaded Google Font
+## To see the results updated, it's needed to call windows() or save the image
+showtext::showtext_opts(dpi = 320)
+showtext::showtext_auto()
+
+# 1. Data download, load and handling
+## Data on inequality comes from IBGE
+## https://www.ibge.gov.br/estatisticas/sociais/populacao/25844-desigualdades-sociais-por-cor-ou-raca.html?=&t=resultados
+rawdata <- read.csv2("2022/week06/data.csv")
+
+## Keeps only income for black people and orders the data by it
+df <- rawdata %>% 
+  dplyr::filter(race == "black") %>% 
+  dplyr::arrange(desc(income))
+
+## Associates each year with a color
+df <- df %>% 
+  dplyr::mutate(color = c("#dc143c","#ffd700","#d2b48c",
+                          "#4682b4","#654321","#d2b48c",
+                          "#000000"))
+
+## Defines size for the circles
+df <- df %>% 
+  dplyr::mutate(size = scales::rescale(income, to = c(200,600)))
+
+## Defines some layout constants
+lnhgt <- 0.9
+bgcolor <- "#d2b48c"
+thick <- 0.05
+wide <- 0.15
+smooth <- 3.3
+radius <- c(9.55,8.5,7.4,6.17,5.37,4.9,3.05)
+theta <- c(1.5,5.8,3.9,0.5,4.9,2.4)
+
+## Defines coordinates for the titles
+titles <- tibble(
+  x = 0,
+  y = c(14.5,11.3,-13.5),
+  size = c(35,12,15),
+  label = c(
+    "REAL MONTHLY INCOME PER CAPITA\nOF BLACK HOUSEHOLDS IN BRAZIL.",
+    "EACH CIRCLE REPRESENTS THE INCOME FOR A GIVEN YEAR. ALL VALUES ARE DEFLATED\nTO 2018 BRAZILIAN REAIS. TRANSPARENT CIRCLES INDICATE YEARS OF INCOME LOSS.",
+    "INSPIRED BY: W.E.B. DU BOIS | DATA FROM: IBGE | GRAPHIC BY: ÍCARO BERNARDES (@IcaroBSC)"
+  )
+)
+
+## Defines coordinates for the message
+message <- tibble(
+  x = 0,
+  y = -11,
+  size = 20,
+  label = "IN THE SAME PERIOD, WHITE HOUSEHOLDS HAD IN THEIR LOWEST AN INCOME OF R$ 1846.31.<br>
+  ALMOST THE <span style='color:white;'>DOUBLE</span> OF BLACK HOUSEHOLDS AT THEIR MAXIMUM (R$ 934.23)."
+)
+
+## Defines coordinates for the income labels
+incomes <- tibble(
+  x = c(8, -2.9, -4.6, 3, -4.3, 2.4, 0),
+  y = c(0, 6.7, -4.25, 4.55, 1.15, -2.97, 0),
+  label = glue::glue("R$ {df$income}"),
+  angle = c(0, 290, 45, 60, 346, 312, 0),
+  color = c("white","black","black",
+            "white","white","black",
+            "white")
+)
+
+## Defines coordinates for the years labels
+years <- tibble(
+  x = 0,
+  y = -radius,
+  label = df$year,
+  color = c("white","black","black",
+            "white","white","black",
+            "white")
+)
+
+## Creates coordinates for the wedges
+plt <- df %>% 
+  dplyr::select(year, color) %>% 
+  dplyr::rename("fill" = "color")
+wedges0 <- tibble(
+  r = radius[1:6]+0.2,
+  start = theta,
+  end = start+wide,
+  year = df$year[1:6]
+)  %>% 
+  dplyr::left_join(plt) %>% 
+  dplyr::mutate(x0 = 0, y0 = 0, r0 = 1.8)
+wedges1 <- tibble(
+  r = radius[2:6]+0.2,
+  start = theta[1:5]-thick,
+  end = start+wide+2*thick,
+  year = df$year[2:6]
+) %>% 
+  dplyr::left_join(plt) %>% 
+  dplyr::mutate(x0 = 0, y0 = 0, r0 = 1.8)
+wedges2 <- tibble(
+  r = radius[3:6]+0.2,
+  start = theta[1:4]-2*thick,
+  end = start+wide+4*thick,
+  year = df$year[3:6]
+) %>% 
+  dplyr::left_join(plt) %>% 
+  dplyr::mutate(x0 = 0, y0 = 0, r0 = 1.8)
+wedges3 <- tibble(
+  r = radius[4:6]+0.2,
+  start = theta[1:3]-3*thick,
+  end = start+wide+6*thick,
+  year = df$year[4:6]
+) %>% 
+  dplyr::left_join(plt) %>% 
+  dplyr::mutate(x0 = 0, y0 = 0, r0 = 1.8)
+wedges4 <- tibble(
+  r = radius[5:6]+0.2,
+  start = theta[1:2]-4*thick,
+  end = start+wide+8*thick,
+  year = df$year[5:6]
+) %>% 
+  dplyr::left_join(plt) %>% 
+  dplyr::mutate(x0 = 0, y0 = 0, r0 = 1.8)
+wedges5 <- tibble(
+  r = radius[6]+0.2,
+  start = theta[1]-5*thick,
+  end = start+wide+10*thick,
+  year = df$year[6]
+) %>% 
+  dplyr::left_join(plt) %>% 
+  dplyr::mutate(x0 = 0, y0 = 0, r0 = 1.8)
+
+# 2. Generates the plot
+## Creates the main plot
+p <- df %>% 
+  ggplot() +
+  
+  ### Places the "target sections"
+  geom_point(aes(x = 0, y = 0, size = I(size), color = I(color))) +
+  
+  ### Places the wedges
+  ggforce::geom_arc_bar(aes(x0 = x0, y0 = y0, r0 = r0, r = r, fill = I(fill),
+                            start = start, end = end), color = NA ,
+                        radius = unit(smooth, 'mm'), data = wedges5) +
+  ggforce::geom_arc_bar(aes(x0 = x0, y0 = y0, r0 = r0, r = r, fill = I(fill),
+                            start = start, end = end), color = NA ,
+                        radius = unit(smooth, 'mm'), data = wedges4) +
+  ggforce::geom_arc_bar(aes(x0 = x0, y0 = y0, r0 = r0, r = r, fill = I(fill),
+                            start = start, end = end), color = NA ,
+                        radius = unit(smooth, 'mm'), data = wedges3) +
+  ggforce::geom_arc_bar(aes(x0 = x0, y0 = y0, r0 = r0, r = r, fill = I(fill),
+                            start = start, end = end), color = NA ,
+                        radius = unit(smooth, 'mm'), data = wedges2) +
+  ggforce::geom_arc_bar(aes(x0 = x0, y0 = y0, r0 = r0, r = r, fill = I(fill),
+                            start = start, end = end), color = NA ,
+                        radius = unit(smooth, 'mm'), data = wedges1) +
+  ggforce::geom_arc_bar(aes(x0 = x0, y0 = y0, r0 = r0, r = r, fill = I(fill),
+                            start = start, end = end), color = NA ,
+                        radius = unit(smooth, 'mm'), data = wedges0) +
+  
+  ### Places the titles
+  geom_text(aes(x = x, y = y, label = label, size = I(size)),
+            family = sans, lineheight = lnhgt, vjust = 1, data = titles) +
+  
+  ### Places the message and the rectangle highlight
+  annotate("tile", x = -5.5, y = -12.1, width = 2.1, height = 0.8, fill = "#dc143c") +
+  ggtext::geom_richtext(aes(x = x, y = y, label = label, size = I(size)),
+                        family = sans, lineheight = lnhgt, vjust = 1,
+                        label.color = NA, fill = NA, data = message) +
+  
+  ### Places the income labels
+  geom_text(aes(x = x, y = y, label = label, color = I(color), angle = angle),
+            size = 12, family = sans, data = incomes) +
+  
+  ### Places the years labels
+  geom_text(aes(x = x, y = y, label = label, color = I(color)),
+            size = 12, family = sans, data = years) +
+  
+  ### Defines a coordinates system and limits the axes
+  coord_cartesian(xlim = c(-11,11), ylim = c(-14,14)) +
+  
+  ### Eliminates unnecessary elements and customizes the plot
+  theme_void() +
+  theme(
+    plot.background = element_rect(fill = bgcolor, color = NA)
+  )
+
+## Saves the plot
+ggsave("2022/week06/income.png", plot = p, dpi = "retina",
+       width = 22, height = 28)
+
+
