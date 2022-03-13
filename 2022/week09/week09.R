2fcbb9ca5b0034ee4541aa4a622a3616d9c98286
Icaro Bernardes <asaicaro@gmail.com>
2022-03-13 13:48:59 -0300

    many weeks...

---
+++ 2022/week09/week09.R
@@ -0,0 +1,316 @@
+# 0. Library and fonts management
+library(tidyverse)
+library(ggplot2)
+library(showtext)
+library(glue)
+library(ggtext)
+library(circlize)
+library(ggplotify)
+library(grid)
+library(cowplot)
+library(patchwork)
+library(countrycode)
+library(rnaturalearth)
+library(sf)
+library(colorspace)
+library(ggimage)
+library(scales)
+
+## Adding Google Fonts
+sysfonts::font_add_google(name = "Merriweather", family = "merriweather") ### Serif
+serif <- "merriweather"
+sysfonts::font_add_google(name = "Raleway", family = "raleway") ### Sans Serif
+sans <- "raleway"
+
+## Defines some layout constants
+waterclr <- "#63A7D4"
+
+# 1. Data download, load and handling
+rawdata <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-03-08/erasmus.csv')
+
+## Gets the abbreviation list and corrects some exceptions
+abbreviation <- tibble(
+  abbrev = unique(c(rawdata$sending_country_code, rawdata$receiving_country_code)),
+  country = countrycode::countrycode(abbrev,
+                                     origin = "iso2c",
+                                     destination = "iso.name.en")
+) %>% 
+  dplyr::mutate(country = case_when(abbrev == "UK" ~ "United Kingdom",
+                                    abbrev == "EL" ~ "Greece",
+                                    abbrev == "XK" ~ "Kosovo",
+                                    TRUE ~ country),
+                name = case_when(country == "Czechia" ~ "Czech Rep.",
+                                 country == "Bosnia and Herzegovina" ~ "Bosnia and Herz.",
+                                 country == "Netherlands (the)" ~ "Netherlands",
+                                 country == "North Macedonia" ~ "Macedonia",
+                                 country == "Moldova (the Republic of)" ~ "Moldova",
+                                 country == "Russian Federation (the)" ~ "Russia",
+                                 country == "Palestine, State of" ~ "Palestine",
+                                 TRUE ~ country)
+  )
+
+## Groups countries according to the statistical regions
+## defined by the UN Statistics Division and some extras
+## https://en.wikipedia.org/wiki/Eastern_Europe#/media/File:Europe_subregion_map_UN_geoscheme.svg
+grouping <- tibble(
+  abbrev = c("TG","PS","DZ","MA","IL",
+             "EG","LB","TN","JO","AZ",
+             "AM","CY",
+             
+             "IS","NO","SE","FI","IE",
+             "DK","LV","LT","EE","UK",
+             
+             "ME","XK","BA","AL","RS",
+             "MK","EL","MT","PT","HR",
+             "SI","ES","TR","IT",
+             
+             "BY","RU","MD","UA","GE",
+             "BG","SK","PL","HU","CZ",
+             "RO",
+             
+             "LI","AT","NL","LU","BE",
+             "DE","FR"),
+  group = c(rep("Africa and Asia",12),
+            rep("Northern Europe",10),
+            rep("Southern Europe",14),
+            rep("Eastern Europe",11),
+            rep("Western Europe",7))
+)
+
+## Unites the grouping and abbreviation tibbles
+sets <- dplyr::left_join(abbreviation, grouping)
+
+## Defines the colors of the regions used to group the countries
+# mycolor <- cols4all::c4a("misc.kelly", n = 9)
+# mycolor <- mycolor[c(3,5,6,7,9)]
+mycolor <- c("#977036", "#dc48c9", "#e55700", "#4c0518", "#ff2267")
+colorgroups <- tibble(
+  group = sort(unique(sets$group)),
+  color = mycolor
+)
+
+## Gets the shapes of the countries
+shapes <- ne_countries(scale = 50, "countries", returnclass = "sf")
+shapes_cnt <- shapes %>% 
+  dplyr::full_join(sets, by = "name") %>% 
+  dplyr::filter(name %in% sets$name) %>% 
+  dplyr::select(country, group, geometry) %>% 
+  dplyr::left_join(colorgroups)
+
+## Only keeps meeting activities data and
+## calculates the number of participants by country
+df <- rawdata %>% 
+  dplyr::filter(activity_mob != "Youth Exchanges - Programme Countries") %>% 
+  dplyr::select(activity_mob, sending_country_code, receiving_country_code, participants) %>% 
+  dplyr::group_by(activity_mob, sending_country_code, receiving_country_code) %>% 
+  dplyr::summarise(participants = sum(participants)) %>% 
+  dplyr::ungroup()
+
+## Applies the grouping and de-abbreviation
+df <- df %>% 
+  dplyr::left_join(sets, by = c("receiving_country_code" = "abbrev")) %>% 
+  dplyr::select(-receiving_country_code) %>% 
+  dplyr::rename_with(.fn = ~glue::glue("receiving_{.}"),
+                     .cols = c("country","group")) %>% 
+  dplyr::left_join(sets, by = c("sending_country_code" = "abbrev")) %>% 
+  dplyr::select(-sending_country_code) %>% 
+  dplyr::rename_with(.fn = ~glue::glue("sending_{.}"),
+                     .cols = c("country","group"))
+
+## Calculates the number of participants in national meetings by host country
+## and only keeps the top countries 
+nation <- df %>% 
+  dplyr::filter(activity_mob == "National youth meetings") %>% 
+  dplyr::group_by(receiving_country) %>% 
+  dplyr::summarise(participants = sum(participants),
+                   group = unique(receiving_group)) %>% 
+  dplyr::ungroup() %>% 
+  dplyr::arrange(desc(participants)) %>% 
+  dplyr::mutate(pct = round(100*cumsum(participants)/sum(participants), 1)) %>% 
+  dplyr::filter(lag(pct, default = 0) <= 75) %>% 
+  dplyr::left_join(colorgroups) %>% 
+  dplyr::mutate(receiving_country = factor(receiving_country),
+                receiving_country = forcats::fct_inorder(receiving_country))
+
+## Calculates the number of participants in
+## international meetings by sending and receiving country
+inter <- df %>% 
+  dplyr::filter(activity_mob == "Transnational youth meetings") %>% 
+  dplyr::group_by(sending_group, receiving_group) %>% 
+  dplyr::summarise(participants = sum(participants)) %>% 
+  dplyr::ungroup()
+
+## Creates a list of the countries by region
+cnt_list <- sets %>% 
+  dplyr::group_by(group) %>% 
+  dplyr::summarise(country = paste(sort(name), collapse = " #")) %>% 
+  dplyr::ungroup() %>% 
+  dplyr::left_join(colorgroups) %>% 
+  dplyr::mutate(group = glue::glue("<span style='color:{color};'>**{group}:**</span>")) %>% 
+  dplyr::select(-color) %>% 
+  tidyr::unite(label, everything(), sep = " #") %>%
+  dplyr::summarise(label = paste(label, collapse = "<br><br>"))
+
+## Defines the coordinates for the titles
+titles <- tibble(
+  x = c(rep(0.5,3),rep(0.95,2),rep(0.35,2),rep(0.4,2)),
+  y = c(0.98,0.93,0.88,0.82,0.8,0.47,0.45,0.21,0.17),
+  size = c(38,10,13,rep(c(15,8),3)),
+  hjust = c(rep(0.5,3),rep(1,2),rep(0,2),rep(1,2)),
+  label = c('**Hallo! Hej! Bok! Moi!**',
+            'ERASMUS is an European Union programme for education, training, youth and sport.<br>
+            Students that take part on it may move to institutions (mostly) across Europe.<br>
+            Two of the many mobility activities are **national** and **transnational** in nature.',
+            'Data from: Data.Europa | Graphic by: Ícaro Bernardes (@IcaroBSC)',
+            '**COUNTRIES REGIONS**',
+            'All countries to which the sending and receiving institutions belong<br>
+            are listed bellow according to their region.',
+            '**NATIONAL MEETINGS**',
+            'Most students that participate in national meetings come from institutions<br>
+            from the hosting country. The bar chart on the left shows the number<br>
+            of participants by receiving country. These countries amount<br>
+            to 75% of all participants in national meetings.<br>
+            The top 2, Germany and Poland, receive a staggering number of students.<br>
+            It is also worth of note the number of visitors that come to<br>
+            the relatively low populated Latvia and Lithuania.',
+            '**TRANSNATIONAL<br>MEETINGS**',
+            'Most students that participate<br>
+            in transnational meetings stay<br>
+            within the same region. Western<br>
+            and Southern Europe send and<br>
+            receive students the most.')
+)
+
+# 2. Generates the plot
+## Defines some parameters for base plots (like circlize ones)
+circos.clear()
+par(
+  mar = rep(0,4), ### Margin around chart
+  bg = NA ### Background color
+) 
+
+## Makes the chord plot
+chordDiagram(
+  x = inter, 
+  grid.col = mycolor,
+  transparency = 0.25,
+  directional = 1,
+  direction.type = c("arrows", "diffHeight"), 
+  diffHeight  = -0.04,
+  annotationTrack = "grid", 
+  annotationTrackHeight = c(0.05, 0.1),
+  link.arr.type = "big.arrow", 
+  link.sort = TRUE, 
+  link.largest.ontop = TRUE)
+
+## Adds axis
+circos.trackPlotRegion(
+  track.index = 1, 
+  bg.border = NA, 
+  panel.fun = function(x, y) {
+    
+    xlim = get.cell.meta.data("xlim")
+    sector.index = get.cell.meta.data("sector.index")
+    
+    circos.axis(
+      h = "top",
+      labels.cex = 1.1
+      )
+  }
+)
+
+## Gets the plot from the device and saves 
+## it in an ggplot-friendly format
+circle <- recordPlot()
+circle <- ggplotify::as.ggplot(cowplot::ggdraw(circle))
+
+## Allows the use of the downloaded Google Font.
+## Causes error if comes before the circular plot
+showtext::showtext_opts(dpi = 320)
+showtext::showtext_auto()
+
+## Creates the bar plot
+bars <- nation %>% 
+  ggplot() +
+  
+  ### Places the bars
+  geom_bar(aes(x = receiving_country, y = participants, fill = I(color)),
+           stat = "identity") +
+  
+  ### Places label for the countries and amount of visitors
+  geom_text(aes(x = receiving_country, y = 0, label = receiving_country),
+            color = "white", hjust = 0, angle = 90,
+            family = serif, size = 8.5, nudge_y = 500, nudge_x = -0.05) +
+  geom_text(aes(x = receiving_country, y = participants, color = I(color),
+                label = scales::label_number_si()(participants)),
+            vjust = 0, family = serif, size = 6, nudge_y = 500) +
+  
+  ### Eliminates unnecessary details
+  theme_void()
+
+## Creates the map
+map <- shapes %>% 
+  ggplot() +
+  
+  ### Places countries in general
+  geom_sf(fill = "#d7dbdb") +
+  
+  ### Places the countries of interest
+  geom_sf(aes(fill = I(color)), data = shapes_cnt) +
+  
+  ### Defines limits for the plot and applies the Mercator projection (default)
+  coord_sf(xlim = c(-25,60), ylim = c(5,70)) +
+  
+  ### Eliminates unnecessary details and customizes the plot
+  theme_void() +
+  theme(
+    panel.background = element_rect(fill = waterclr, color = NA)
+  )
+
+p <- ggplot(NULL) + 
+  
+  ### Places the titles
+  ggtext::geom_richtext(aes(x = x, y = y, label = label,
+                            size = I(size), hjust = hjust),
+                        vjust = 1, fill = NA, label.colour = NA,
+                        family = sans, data = titles) +
+  
+  ### Places lines and stars to separate sections
+  annotate("segment", x = 0.05, xend = 0.95, y = 0.84, yend = 0.84,
+           color = "#E4BD00", size = 8, lineend = "round") +
+  annotate("point", x = 0.5, y = 0.84, size = 50,
+           color = colorspace::lighten(waterclr, 0.85)) +
+  ggimage::geom_image(aes(x = 0.5, y = 0.84,
+                          image = "https://cdn-icons-png.flaticon.com/512/197/197615.png"),
+                      size = 0.05, by = "width", asp = 0.5) +
+  
+  ### Places geometric forms to highlight the map section
+  annotate("point", x = 0.1, y = 0.63, size = 390,
+           color = colorspace::darken(waterclr, 0.3)) +
+  annotate("rect", xmin = 0, xmax = 1, ymin = 0.51-0.015, ymax = 0.75+0.015,
+           fill = colorspace::darken(waterclr, 0.3), color = NA) +
+  annotate("point", x = 0.1, y = 0.63, size = 350, color = waterclr) +
+  annotate("rect", xmin = 0, xmax = 1, ymin = 0.51, ymax = 0.75,
+           fill = waterclr, color = NA) +
+  
+  ### Places the countries groups list
+  ggtext::geom_textbox(aes(x = 0.475, y = 0.63, label = label), hjust = 0,
+                       color = "white", box.colour = NA, fill = NA, size = 8,
+                       width = unit(9, "inch"), family = serif, data = cnt_list) +
+  
+  coord_cartesian(xlim = c(0,1), ylim = c(0,1), expand = FALSE) +
+  theme_void() +
+  theme(
+    panel.background = element_rect(fill = colorspace::lighten(waterclr, 0.85), color = NA)
+  ) +
+  inset_element(map, left = 0.015, right = 0.45,
+                bottom = 0.51, top = 0.755) +
+  inset_element(bars, left = 0.03, right = 0.47,
+                bottom = 0.26, top = 0.52) +
+  inset_element(circle, left = 0.45, right = 0.98,
+                bottom = 0.02, top = 0.27)
+
+## Saves the plot
+ggsave("2022/week09/erasmus.png", plot = p, dpi = "retina",
+       width = 18, height = 35)
+
---
+++ 2022/week10/dubouis06.R
@@ -0,0 +1,168 @@
+# 0. Library and fonts management
+library(tidyverse)
+library(ggplot2)
+library(showtext)
+library(glue)
+library(scales)
+library(ggforce)
+
+## Adding Google Fonts
+sysfonts::font_add_google(name = "Teko", family = "teko") ### Sans Serif
+sans <- "teko"
+
+## Allows the use of the downloaded Google Font
+## To see the results updated, it's needed to call windows() or save the image
+showtext::showtext_opts(dpi = 320)
+showtext::showtext_auto()
+
+## Defines some layout constants
+r <- 0.05 ### To set the bars size
+r1 <- 0.005 ### To set the minor vertical bars size
+mult <- 0.9 ### To make the intersection between bars
+Vlim <- 1 ### To define the distance to the Y-axis
+Hlim <- 1.4 ### To define the distance to the X-axis
+lnhgt <- 0.7 ### To set the lineheight
+bgcolor <- "#d2b48c"
+
+# 1. Data download, load and handling
+## Data on inequality comes from IBGE
+## https://www.ibge.gov.br/estatisticas/sociais/populacao/25844-desigualdades-sociais-por-cor-ou-raca.html?=&t=resultados
+# rawdata <- read.csv2("2022/week10/data.csv")
+
+## Keeps only data of illiteracy for black people
+df <- rawdata %>% 
+  dplyr::filter(race == "black")
+
+## Adds an estimate for 2019
+df <- df %>% 
+  tibble::add_row(year = 2019, race = "black", illiteracy = 9.0)
+
+## Rescales the data to a 1-2 range
+df <- df %>% 
+  dplyr::mutate(x = scales::rescale(illiteracy, to = c(1,2)),
+                y = scales::rescale(year, to = c(1,2)))
+
+## Defines the coordinates for the vertical bars
+Vbars <- df %>% 
+  dplyr::transmute(ymax = max(y)*Hlim,
+                   ymin = y*mult,
+                   xmin = x*mult,
+                   xmax = xmin - r)
+
+## Defines the coordinates for the (major) horizontal bars
+Hbars_mjr <- df %>% 
+  dplyr::transmute(xmax = max(x)*Vlim,
+                   xmin = x*mult,
+                   ymin = y*mult,
+                   ymax = ymin - r)
+
+## Defines the coordinates for the (minor) horizontal bars
+Hbars_mnr <- df %>% 
+  dplyr::transmute(xmax = max(x)*Vlim - r1,
+                   xmin = x*mult,
+                   ymin = y*mult - r1,
+                   ymax = ymin - r + 2*r1)
+
+## Defines the coordinates for the circular sections
+circles <- df %>% 
+  dplyr::transmute(x0 = x*mult,
+                   y0 = y*mult,
+                   r0 = 0,
+                   r = r,
+                   r1 = r1,
+                   r2 = r - r1)
+
+## Defines the coordinates for the x-axis labels
+Xlabels <- df %>% 
+  dplyr::transmute(y = max(y)*Hlim,
+                   x = x*mult - r/2,
+                   label = glue::glue("{format(illiteracy, nsmall = 1)}%")) %>% 
+  dplyr::mutate(label = ifelse(row_number() == n(),
+                               glue::glue("({label}?)"),
+                               label))
+
+## Defines the coordinates for the x-axis labels
+Ylabels <- df %>% 
+  dplyr::transmute(x = max(x)*Vlim,
+                   y = y*mult - r/2,
+                   label = year,
+                   size = 22) %>% 
+  dplyr::mutate(label = ifelse(row_number() == n(),
+                               glue::glue("({label}?)"),
+                               label)) %>% 
+  tibble::add_row(x = max(df$x)*Vlim,
+                  y = max(df$y)*Hlim+0.02,
+                  label = "PERCENT OF\nILLITERACY",
+                  size = 12)
+
+## Defines the coordinates for the titles
+titles <- tibble(
+  x = 1.45,
+  y = c(0.56,0.7,2.95),
+  label = c("ILLITERACY RATE OF BLACK PEOPLE IN BRAZIL.",
+            "ILLITERACY RATE AMONG PEOPLE AGED 15 OR OLDER (2018).",
+            "INSPIRED BY: W.E.B. DU BOIS | DATA FROM: IBGE | GRAPHIC BY: ÍCARO BERNARDES (@IcaroBSC)"),
+  size = c(35,14,16)
+)
+
+## Defines the coordinates for the message
+message <- tibble(
+  x = 0.7,
+  y = c(0.93,1.18),
+  label = c("31.8%",
+            "OF BLACKS AGED 65 OR OLDER\nWERE ILLITERATE IN 2018.\nILLITERATE WHITES IN\nTHE SAME AGE RANGE\nWERE ONLY 12.3%."),
+  size = c(80,16),
+  color = "#654321"
+)
+
+# 2. Generates the plot
+## Creates the main plot
+p <- ggplot(NULL) +
+  
+  ### Places the bars
+  geom_rect(aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax),
+            fill = "black", data = Vbars) + #### Black vertical bars
+  geom_rect(aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax),
+            fill = "black", data = Hbars_mjr) + #### Black horizontal bars
+  geom_rect(aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax),
+            fill = bgcolor, data = Hbars_mnr) + #### Beige horizontal bars
+  
+  ### Places the circle arcs
+  ggforce::geom_arc_bar(aes(x0 = x0, y0 = y0, r0 = r0, r = r, start = pi/4, end = pi/2),
+                        color = NA, fill = "black", data = circles) + #### Black lower arc
+  ggforce::geom_arc_bar(aes(x0 = x0, y0 = y0, r0 = r0, r = r, start = 0, end = pi/4),
+                        color = NA, fill = "black", data = circles) + #### Black upper arc
+  ggforce::geom_arc_bar(aes(x0 = x0, y0 = y0, r0 = r1, r = r2, start = 0, end = pi/4),
+                        color = NA, fill = bgcolor, data = circles) + #### Beige upper arc
+  
+  ### Places the axes labels
+  geom_text(aes(x = x, y = y, label = label), size = 12,
+            vjust = 1, nudge_y = -0.02, family = sans, data = Xlabels) +
+  geom_text(aes(x = x, y = y, label = label, size = I(size)),
+            nudge_x = -0.1, family = sans, lineheight = lnhgt, data = Ylabels) +
+  
+  ### Places the titles
+  geom_text(aes(x = x, y = y, label = label, size = I(size)),
+            vjust = 1, family = sans, data = titles) +
+  
+  ### Places the message
+  geom_text(aes(x = x, y = y, label = label, size = I(size), color = I(color)),
+            hjust = 1, family = sans, lineheight = lnhgt, data = message) +
+  
+  ### Reverses and sets limits for the axes and eliminates axis expansion
+  scale_x_reverse(limits = c(2.35,0.55), expand = expansion(0,0)) + 
+  scale_y_reverse(limits = c(3.1,0.45), expand = expansion(0,0)) +
+  
+  ### Ensures that the proportions between axes are equal
+  coord_equal(expand = FALSE) +
+  
+  ### Eliminates non-necessary elements and customizes the plot
+  theme_void() +
+  theme(
+    plot.background = element_rect(fill = bgcolor, color = NA)
+  )
+
+## Saves the plot
+ggsave("2022/week10/illiteracy.png", plot = p, dpi = "retina",
+       width = 22, height = 28)
+
